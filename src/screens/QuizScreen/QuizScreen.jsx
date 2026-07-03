432b365ac880a8fc4e7ef88f7ded1420a474b110
jaesu968 <mazterk968@gmail.com>
2026-07-02 19:31:38 -0500

    fixed styling inconsistencies and added ArrowRightIcon component for added consistency

--- src/screens/QuizScreen/QuizScreen.jsx
+++ src/screens/QuizScreen/QuizScreen.jsx
@@ -30,6 +30,7 @@ import styles from './QuizScreen.module.css';
 import TimerBar from '../../components/TimerBar';
 import clockIcon from '../../assets/clock-icon.svg';
 import Button from '../../components/Button';
+import ArrowRightIcon from '../../components/ArrowRightIcon';
 
 function QuizScreen({
   selectedTopic,
@@ -125,9 +126,7 @@ function QuizScreen({
       )}
       <div ref={nextButtonRef}>
         <Button variant="primary" disabled={!selectedAnswer} onClick={handleNextQuestion}>
-          Next Question <svg xmlns="http://www.w3.org/2000/svg" width="15" height="15" fill="currentColor" className="bi bi-arrow-right" viewBox="0 0 16 16">
-            <path fillRule="evenodd" d="M1 8a.5.5 0 0 1 .5-.5h11.793l-3.147-3.146a.5.5 0 0 1 .708-.708l4 4a.5.5 0 0 1 0 .708l-4 4a.5.5 0 0 1-.708-.708L13.293 8.5H1.5A.5.5 0 0 1 1 8" />
-          </svg>
+          Next Question <ArrowRightIcon />
         </Button>
       </div>
     </ScreenLayout>
--- src/screens/ResultsScreen/ResultsScreen.jsx
+++ src/screens/ResultsScreen/ResultsScreen.jsx
@@ -2,22 +2,26 @@ import styles from './ResultsScreen.module.css';
 import logo from '../../assets/logo-desktop-on-light.svg';
 import Button from '../../components/Button';
 import ResultsCard from '../../components/ResultsCard';
-import ScreenLayout from '../../components/ScreenLayout/ScreenLayout';
+import ScreenLayout from '../../components/ScreenLayout';
+import ArrowRightIcon from '../../components/ArrowRightIcon';
 
 function ResultsScreen({ score, totalQuestions, onReturnHome }) {
   
   return (
     <ScreenLayout>
+      <header className={styles.header}>
       <img src={logo} alt="devquiz" className={styles.logo} />
-      <h1 className={styles.headerTitle}>Quiz Complete</h1>
+      </header>
 
-      <ResultsCard score={score} totalQuestions={totalQuestions} />
-
-      <div className={styles.actions}>
-        <Button onClick={onReturnHome}>
-          Return Home →
-        </Button>
-      </div>
+      <main className={styles.quizContent}> 
+        <h1 className={styles.headerTitle}>Quiz Complete</h1>
+        <ResultsCard score={score} totalQuestions={totalQuestions} />
+        <div className={styles.actions}>
+          <Button variant="primary" onClick={onReturnHome}>
+            Return Home <ArrowRightIcon />
+          </Button>
+        </div>
+      </main>
     </ScreenLayout>
   );
 }
